0a4ce26e5d88b422fbb318ec5dd66c04e7fee20b
SooryaSubramani <sooryasubramani5@gmail.com>
2025-12-19 16:06:20 +0530

    Configure React app for deployment to GitHub Pages

--- src/components/react_Headder.js
+++ src/components/react_Headder.js
@@ -20,7 +20,7 @@ function Reactheadder() {
     
      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
        {/* <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none"> */}
-         <img  width="40" height="32"  role="img" src={logo} className="App-logo" alt="logo" />
+         <img  width="40" height="32"  role="img" src={logo} alt="logo" />
           <img  width="40" height="32"  src={html} alt="logo" />
           <img  width="40" height="32"  src={css} alt="logo" />
           <img  width="40" height="32"  src={javascript} alt="logo" />
